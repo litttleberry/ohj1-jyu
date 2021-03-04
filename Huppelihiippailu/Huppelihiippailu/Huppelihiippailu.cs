7ab52976417dd3a646d7dd313f6ce411aaa96209
Marjanen Sanni <sanni.e.marjanen@student.jyu.fi>
2021-03-04 20:23:16 +0200

    Kommentit, reunat, törmäys uusiks, vähän vähemmän epätoivoa

--- Huppelihiippailu/Huppelihiippailu/Huppelihiippailu.cs
+++ Huppelihiippailu/Huppelihiippailu/Huppelihiippailu.cs
@@ -9,19 +9,21 @@ using System.Collections.Generic;
 /* TO DO
  * grafiikat (taustat, objektit)
  * esteiden paikat / liikkuminen? 
- * osuma reunaan/väärä suunta > siirrä B. alkuun
+ * osuma reunaan/väärä suunta > miten Balkuun, ei koko kenttää alusta?
  * "kyltit"  < perikato | koti >
  * kotiinpääsy/maali
  * pelin päättyminen?
- *      G.O. uusi screen "wasted"
- *      voitto uusi screen + grafiikat
+ *      G.O. uusi screen "wasted", KrapulaVoitti-aliohjelma
+ *      voitto uusi screen + grafiikat, SelvisitMaaliin-aliohjelma
  * laskuri toimimaan -- aika ja ++/-- osumat
  * nice-to-have: pensasasiat piilopullojen eteen
  * nice-to-have: naapurit (vihu)
  * nice-to-have: humalainen kävely
+ * MISTÄ: SILMUKKA, TAULUKKO? Tilasto?
  */
 
-
+/// @author saelmarj
+/// @version 4.3.2021
 /// <summary>
 /// Huppelihiippailu-peli
 /// </summary>
@@ -35,22 +37,28 @@ public class Huppelihiippailu : PhysicsGame
 
     DoubleMeter krapulamittari;
     
+    /// <summary>
+    /// Peli aloitus tää vissiin on tää Begin?
+    /// </summary>
     public override void Begin()
     {
         LuoKentta();
         LuoKrapulamittari();
-
     }
 
+
+    /// <summary>
+    /// Luodaan pisteitä JA JOS ONNISTUN NIIN AIKAA mittaava laskuri.
+    /// </summary>
     public void LuoKrapulamittari()
     {
-        krapulamittari = new DoubleMeter(7); // lähtöarvo
-        krapulamittari.MaxValue = 15;
+        krapulamittari = new DoubleMeter(5); // mittarin lähtöarvo
+        krapulamittari.MaxValue = 10;
         krapulamittari.LowerLimit += KrapulaVoitti;
 
 
         Label otsikko = new Label("Hilpeysmittari");
-        otsikko.X = Screen.Left + 100;
+        otsikko.X = Screen.Left + 120;
         otsikko.Y = Screen.Top - 120;
         Add(otsikko);
 
@@ -62,20 +70,25 @@ public class Huppelihiippailu : PhysicsGame
         hilpeystaso.Color = Color.Silver;
         hilpeystaso.BindTo(krapulamittari);
         Add(hilpeystaso);
-
-
-
     }
 
     
+    /// <summary>
+    /// TÄSTÄ PITÄIS SAADA TEHTYÄ SELLANEN GAME OVER -ALIOHJELMA.
+    /// </summary>
     public void KrapulaVoitti()
     {
-         MessageDisplay.Add("O ou, taisi laskuhumala viedä voimat.");
+         MessageDisplay.Add("O ou, taisi laskuhumala viedä voimat. \n Peli päättyi!");
     }
 
+
+    /// <summary>
+    /// Luodaan pelikenttä tekstitiedostosta,
+    /// määritetään peli-ikkunan koko ja kamera.
+    /// </summary>
     public void LuoKentta()
     {
-        SetWindowSize(1550, 1050);
+        SetWindowSize(1450, 900/*, true*/);
 
         TileMap kentta = TileMap.FromLevelAsset("kentta");
         kentta.SetTileMethod('-', LuoNurmikko);
@@ -85,19 +98,25 @@ public class Huppelihiippailu : PhysicsGame
         kentta.SetTileMethod('P', LuoTalo, Color.DarkBrown); // pubi eli lähtö
         kentta.SetTileMethod('K', LuoTalo, Color.DarkRed); // koti eli maali
         kentta.SetTileMethod('N', LuoTalo, Color.Charcoal); // naapuri
+        kentta.SetTileMethod('x', LuoReunat);
+        kentta.Optimize('-');
         kentta.Execute(RUUDUN_LEVEYS, RUUDUN_KORKEUS);
 
-        Level.CreateBorders();
         Level.BackgroundColor = Color.LightGray;
 
-        Camera.ZoomToAllObjects();
-        // Camera.StayInLevel = true;
-        // Camera.Zoom(2);
-        // Camera.Follow(ukkeli);
-
+        Camera.ZoomToLevel();
+        Camera.StayInLevel = true;
+      // Camera.Zoom(2.7);
+      // Camera.Follow(ukkeli);
     }
 
     
+    /// <summary>
+    /// Luodaan salaiset yöpalat, joita hahmo kerää matkallaan kotiin.
+    /// </summary>
+    /// <param name="paikka">Paikka</param>
+    /// <param name="leveys">Leveys</param>
+    /// <param name="korkeus">Korkeus</param>
     public void LuoSnack(Vector paikka, double leveys, double korkeus)
     {
         PhysicsObject snack = new PhysicsObject(30, 30);
@@ -109,34 +128,47 @@ public class Huppelihiippailu : PhysicsGame
     }
 
 
+    /// <summary>
+    /// Luodaan esteet, joihin pelihahmo voi törmätä matkallaan kotiin.
+    /// </summary>
+    /// <param name="paikka">Paikka</param>
+    /// <param name="leveys">Leveys</param>
+    /// <param name="korkeus">Korkeus</param>
     public void LuoEste(Vector paikka, double leveys, double korkeus)
     {
         PhysicsObject este =  new PhysicsObject(30, 30);
         este.Position = paikka;
-        este.Shape = Shape.Hexagon;
-        este.Color = Color.Navy;
+        este.Color = Color.Azure;
+        este.Shape = RandomGen.NextShape();
         este.Tag = "este";
         este.MakeStatic();
         Add(este);
     }
     
 
+    /// <summary>
+    /// Luodaan kentän taustana toimiva nurmialue
+    /// </summary>
+    /// <param name="paikka">Paikka</param>
+    /// <param name="leveys">Leveys</param>
+    /// <param name="korkeus">Korkeus</param>
     public void LuoNurmikko(Vector paikka, double leveys, double korkeus)
     {
         PhysicsObject nurmikko = PhysicsObject.CreateStaticObject(leveys, korkeus);
         nurmikko.Position = paikka;
         nurmikko.Shape = Shape.Rectangle;
         nurmikko.Color = Color.DarkJungleGreen;
+        nurmikko.CollisionIgnoreGroup = 1;
         Add(nurmikko);
     }
 
 
     /// <summary>
-    /// Luodaan kentällä olevat staattiset rakennukset
+    /// Luodaan kentällä olevat paikallaan pysyvät rakennukset
     /// </summary>
     /// <param name="paikka">Paikka</param>
-    /// <param name="leveys">Korkeus</param>
-    /// <param name="korkeus">Leveys</param>
+    /// <param name="leveys">Leveys</param>
+    /// <param name="korkeus">Korkeus</param>
     /// <param name="vari">Väri</param>
     public void LuoTalo(Vector paikka, double leveys, double korkeus, Color vari)
     {
@@ -144,15 +176,30 @@ public class Huppelihiippailu : PhysicsGame
         talo.Position = paikka;
         talo.Shape = Shape.Rectangle;
         talo.Color = vari;
-        talo.Tag = "rakennus";
+        talo.CollisionIgnoreGroup = 1;
         Add(talo);
     }
-    
+
+
+    /// <summary>
+    /// Luodaan kentälle reunat, joihin voi törmätä.
+    /// </summary>
+    /// <param name="paikka">Paikka</param>
+    /// <param name="leveys">Leveys</param>
+    /// <param name="korkeus">Korkeus</param>
+    public void LuoReunat(Vector paikka, double leveys, double korkeus)
+    {
+        PhysicsObject reunat = new PhysicsObject(0.1, 0.1);
+        reunat.Position = paikka;
+        reunat.Tag = "reunat";
+        Add(reunat);
+    }
+
 
     /// <summary>
     /// Luodaan pelattava hahmo
     /// </summary>
-    /// <param name="paikka">Lähtöpaikka</param>
+    /// <param name="paikka">Hahmon lähtöpaikka</param>
     /// <param name="leveys">Hahmon leveys</param>
     /// <param name="korkeus">Hahmon korkeus</param>
     public void LuoUkkeli(Vector paikka, double leveys, double korkeus)
@@ -165,25 +212,67 @@ public class Huppelihiippailu : PhysicsGame
         Add(ukkeli);
 
         AsetaOhjaimet();
-        AddCollisionHandler(ukkeli, "este", PelaajaOsuuEsteeseen);
-        AddCollisionHandler(ukkeli, "snack", PelaajaKeraaHerkun);
+        //AddCollisionHandler(ukkeli, "este", PelaajaOsuuEsteeseen);
+        //AddCollisionHandler(ukkeli, "snack", PelaajaKeraaHerkun);
+        AddCollisionHandler(ukkeli, PelaajaTormasi);
     }
-    
 
-    public void PelaajaOsuuEsteeseen(PhysicsObject ukkeli, PhysicsObject kohde)
-    {
-        MessageDisplay.Add("Osuit!");
 
-        krapulamittari.Value --;
-        
-    }
-    
-    public void PelaajaKeraaHerkun(PhysicsObject ukkeli, PhysicsObject kohde)
+    /// <summary>
+    /// Aliohjelma pelihahmon ja muiden kohteiden törmäyksen käsittelyyn.
+    /// </summary>
+    /// <param name="ukkeli"> Pelihahmo </param>
+    /// <param name="kohde"> Kohde, johon törmättiin </param>
+    public void PelaajaTormasi(PhysicsObject ukkeli, PhysicsObject kohde)
     {
-        krapulamittari.Value++;
-        kohde.Destroy();
+        if (kohde.Tag.ToString() == "este")
+        {
+            Label osuitEsteeseen = new Label("Hups, kompuroit!");
+            osuitEsteeseen.Position = new Vector(Screen.Left + 120, Screen.Top - 180);
+            osuitEsteeseen.Color = Color.DarkJungleGreen;
+            osuitEsteeseen.TextColor = Color.Black;
+            osuitEsteeseen.LifetimeLeft = TimeSpan.FromSeconds(0.8);
+            Add(osuitEsteeseen);
+            
+            krapulamittari.Value--;
+
+            List<PhysicsObject> osututEsteet = new List<PhysicsObject>();
+            osututEsteet.Add(kohde);
+        }
+
+        if (kohde.Tag.ToString() == "snack")
+        {
+            krapulamittari.Value++;
+            kohde.Destroy();
+        }
+
+        if (kohde.Tag.ToString() == "reunat")
+        {
+            ukkeli.Stop();  // ei toimi??
+
+            Label osuitReunaan = new Label(RUUDUN_LEVEYS * 20, RUUDUN_KORKEUS * 5);
+            osuitReunaan.Position = new Vector(0, 0);
+            osuitReunaan.Color = Color.DarkJungleGreen;
+            osuitReunaan.TextColor = Color.Black;
+            osuitReunaan.BorderColor = Color.Silver;
+            osuitReunaan.Text = "Hups! Taisit eksyä. Peli alkaa alusta tuokion kuluttua.";
+            Add(osuitReunaan);
+
+            Timer.SingleShot(5.0, AloitaAlusta);
+        }
     }
-    
+
+
+    /// <summary>
+    /// Tyhjentää pelin ja alustaa sen alkamaan alusta.
+    /// EI TOSIN TOIMI NIINKUIN OIS KIVA, zoom pielessä?
+    /// </summary>
+    public void AloitaAlusta()
+    {
+        ClearAll();
+        LuoKentta();
+        LuoKrapulamittari();
+    }
 
 
     /// <summary>
@@ -215,6 +304,5 @@ public class Huppelihiippailu : PhysicsGame
     {
         ukkeli.Velocity = suunta;
     }
-
 
 }
